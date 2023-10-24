2a41ad8b7c6ca0f6cf48ce99dad18b7794a55c98
Joel Emmanuel <ejoel0035@gmail.com>
2023-10-24 11:11:08 +0100

    feat: setup user schema in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from 'src/schemas/user.schema';
 
-@Module({})
+@Module({
+    imports: [
+
+        MongooseModule.forFeature([{name: 'User', schema: userSchema}])
+
+    ],
+    controllers: [AuthController],
+    providers: [AuthService]
+})
+
 export class AuthModule {}
