d2b4817c1f88356468fd0ecf6014be2945e1ba87
Joel Emmanuel <ejoel0035@gmail.com>
2023-10-24 11:28:31 +0100

    build: install jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { userSchema } from 'src/schemas/user.schema';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
     imports: [
+
+        PassportModule.register({defaultStrategy: 'jwt'}),
 
         MongooseModule.forFeature([{name: 'User', schema: userSchema}])
 
