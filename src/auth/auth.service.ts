ac0ff546b9fbc36fbdb0850bcc25500a2b853d41
Joel Emmanuel <ejoel0035@gmail.com>
2023-10-24 16:17:49 +0100

    feat: setup jwt to with signup function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from 'src/schemas/user.schema';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import {bcrypt} from 'bcryptjs';
+
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectModel(User.name)
+        private userModel: Model<User>,
+        private jwtService: JwtService
+    ) {}
+
+
+
+async signUp(signUpDto): Promise<{token: string}> {
+    const {name, email, password} = signUpDto
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userModel.create({
+        name,
+        email,
+        password: hashPassword
+    });
+
+    const token = this.jwtService.sign({id: user._id})
+
+    return {token}
+}
+
+}
+
